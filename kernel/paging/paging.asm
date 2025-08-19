66b6484885e9be180d2d70f465861b7ddc6cc1da
Fabrice <schaubfabrice@gmail.com>
2025-08-19 19:43:39 +0200

    works kinda

--- kernel/paging/paging.asm
+++ kernel/paging/paging.asm
@@ -9,6 +9,11 @@ global page_directory_entries
 global page_table_entries
 global bootstrap_page_table_entries
 
+; HHDM page tables for 4KB pages - need full hierarchy
+global hhdm_page_directory_pointer_entries
+global hhdm_page_directory_entries
+global hhdm_page_table_entries
+
 ALIGN 0x1000
 page_map_level_4_entries:
     times 512 resq 1
@@ -16,7 +21,6 @@ page_map_level_4_entries:
 ALIGN 0x1000
 page_directory_pointer_entries:
     times 512 resq 1
-
 
 ALIGN 0x1000
 page_directory_entries:
@@ -29,3 +33,16 @@ page_table_entries:
 ALIGN 0x1000
 bootstrap_page_table_entries:
     times 512 resq 1
+
+; HHDM page tables for 4KB mapping
+ALIGN 0x1000
+hhdm_page_directory_pointer_entries:
+    times 512 resq 1
+
+ALIGN 0x1000  
+hhdm_page_directory_entries:
+    times 1024 resq 1    ; 2GB worth of PD entries (4 x 512)
+
+ALIGN 0x1000
+hhdm_page_table_entries:
+    times 524288 resq 1  ; 2GB worth of 4KB pages (1024 * 512)
